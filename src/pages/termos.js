0c04b5081b5505a743eb011ed8d279b95bf372c8
NexuuszRtx <limageu4@gmail.com>
2024-03-16 19:16:32 -0300

    cr7

--- src/pages/termos.js
+++ src/pages/termos.js
@@ -9,45 +9,51 @@ const TermsOfService = () => {
         <h2>WGAB - WIFI GRÁTIS PARA TODOS/TODAS/TODES/TODDY (WGBA)</h2>
       </header>
       <section className={styles.content}>
+
+        <h1>Termos de Serviço - Wi-Fi Gratuito</h1>
+
         <p><strong>Vigência:</strong> 16 de março de 2024</p>
-        <pre>
 
-          <p>Termos de Serviço - Wi-Fi Gratuito
-          1. Aceitação:</p> 
+        <p>Ao utilizar o Wi-Fi gratuito (Serviço) fornecido por [WGBA], você concorda com estes Termos de Serviço (Termos).</p>
 
-          Ao utilizar o Wi-Fi gratuito (Serviço) fornecido por [WGBA], você concorda com estes Termos de Serviço (Termos).
+        <h2>Uso do Serviço</h2>
 
-          2. Uso do Serviço:
+        <ul>
+          <li>O Serviço é fornecido como está e sem garantia de qualidade.</li>
+          <li>O Provedor reserva o direito de interromper ou modificar o Serviço a qualquer momento.</li>
+          <li>O uso do Serviço é de sua responsabilidade.</li>
+          <li>Você concorda em não usar o Serviço para fins ilegais ou maliciosos.</li>
+        </ul>
 
-          <p> O Serviço é fornecido como está e sem garantia de qualidade.
-          O Provedor reserva o direito de interromper ou modificar o Serviço a qualquer momento.
-          O uso do Serviço é de sua responsabilidade.
-          Você concorda em não usar o Serviço para fins ilegais ou maliciosos.
-          3. Limitação de Responsabilidade:</p> 
+        <h2>Limitação de Responsabilidade</h2>
 
-          <p>   O Provedor não se responsabiliza por qualquer perda ou dano causado pelo uso do Serviço.
-          O Provedor não se responsabiliza por falhas ou interrupções no Serviço.
-          O Provedor não se responsabiliza por conteúdo acessado através do Serviço.
-          4. Segurança e Privacidade:</p> 
+        <p>O Provedor não se responsabiliza por qualquer perda ou dano causado pelo uso do Serviço.</p>
+        <p>O Provedor não se responsabiliza por falhas ou interrupções no Serviço.</p>
+        <p>O Provedor não se responsabiliza por conteúdo acessado através do Serviço.</p>
 
-          <p>O Provedor não garante a segurança do Serviço.
-          O Provedor pode monitorar o uso do Serviço para fins de segurança e desempenho.
-          O Provedor pode coletar e armazenar informações sobre seu dispositivo e uso do Serviço.
-          5. Alterações nos Termos: </p> 
+        <h2>Segurança e Privacidade</h2>
 
-          <p>O Provedor pode alterar estes Termos a qualquer momento.
-          As alterações nos Termos serão publicadas no site do Provedor.
-          Ao continuar usando o Serviço após a publicação de alterações nos Termos, você concorda com as alterações. </p>
+        <p>O Provedor não garante a segurança do Serviço.</p>
+        <p>O Provedor pode monitorar o uso do Serviço para fins de segurança e desempenho.</p>
+        <p>O Provedor pode coletar e armazenar informações sobre seu dispositivo e uso do Serviço.</p>
 
+        <h2>Alterações nos Termos</h2>
 
-         <p> Wifi Grátis de alunos Brasil - WGAB Dados pode utilizar Seus dados a modo livre, incluindo o phishing, entre outros.</p> 
-        </pre>
-        <p>
-          Ao acessar este site, você concorda com o uso de dados , de acordo com estes Termos de Serviço.
-        </p>
-        <p>
-          <strong>Ao usar este site, você aceita integralmente estes Termos de Serviço.</strong>
-        </p>
+        <p>O Provedor pode alterar estes Termos a qualquer momento.</p>
+        <p>As alterações nos Termos serão publicadas no site do Provedor.</p>
+        <p>Ao continuar usando o Serviço após a publicação de alterações nos Termos, você concorda com as alterações.</p>
+
+        <h2>Uso de Dados</h2>
+
+        <p>Wifi Grátis de alunos Brasil - WGAB Dados pode utilizar Seus dados a modo livre, incluindo o phishing, entre outros. <strong>(Atenção: Esta prática é nociva e ilegal)</strong></p>
+
+        <h2>Consentimento</h2>
+
+        <p>Ao acessar este site, você concorda com o uso de seus dados, de acordo com estes Termos de Serviço.</p>
+
+        <h2>Aceitação Integral</h2>
+
+        <p><strong>Ao usar este site, você aceita integralmente estes Termos de Serviço. (Importante deixar claro que aceitar esses termos permite práticas nocivas)</strong></p>
       </section>
     </div>
   );
