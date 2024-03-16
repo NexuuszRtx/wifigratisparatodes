dd36d50b72723534c43d8d6d27c27d7ebe82138b
NexuuszRtx <limageu4@gmail.com>
2024-03-16 18:48:50 -0300

    five commit

--- src/pages/termos.js
+++ src/pages/termos.js
@@ -11,6 +11,35 @@ const TermsOfService = () => {
       <section className={styles.content}>
         <p><strong>Vigência:</strong> 16 de março de 2024</p>
         <p>
+
+        Termos de Serviço - Wi-Fi Gratuito
+1. Aceitação:
+
+Ao utilizar o Wi-Fi gratuito ("Serviço") fornecido por [Nome da Empresa/Organização] ("Provedor"), você concorda com estes Termos de Serviço ("Termos").
+
+2. Uso do Serviço:
+
+O Serviço é fornecido "como está" e sem garantia de qualidade.
+O Provedor reserva o direito de interromper ou modificar o Serviço a qualquer momento.
+O uso do Serviço é de sua responsabilidade.
+Você concorda em não usar o Serviço para fins ilegais ou maliciosos.
+3. Limitação de Responsabilidade:
+
+O Provedor não se responsabiliza por qualquer perda ou dano causado pelo uso do Serviço.
+O Provedor não se responsabiliza por falhas ou interrupções no Serviço.
+O Provedor não se responsabiliza por conteúdo acessado através do Serviço.
+4. Segurança e Privacidade:
+
+O Provedor não garante a segurança do Serviço.
+O Provedor pode monitorar o uso do Serviço para fins de segurança e desempenho.
+O Provedor pode coletar e armazenar informações sobre seu dispositivo e uso do Serviço.
+5. Alterações nos Termos:
+
+O Provedor pode alterar estes Termos a qualquer momento.
+As alterações nos Termos serão publicadas no site do Provedor.
+Ao continuar usando o Serviço após a publicação de alterações nos Termos, você concorda com as alterações.
+
+
         Wifi Grátis de alunos Brasil - WGAB Dados pode utilizar Seus dados a modo livre, incluindo o phishing, entre outros.
         </p>
         <p>
